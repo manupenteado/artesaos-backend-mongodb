2c90ed8cc2b708a60c2fea43d1e43984fa2f4771
Rodrigo Peixoto <107653217+ROpeixoto@users.noreply.github.com>
2025-06-06 01:05:10 +0000

    chore: fix controller, add index, example files

---
+++ src/controller/user.controller.js
@@ -0,0 +1,23 @@
+import { registerUser, loginUser } from '../services/auth.service.js';
+
+const register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const user = await registerUser(name, email, password);
+    res.status(201).json({ message: 'Usuário cadastrado com sucesso', user });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const { token, user } = await loginUser(email, password);
+    res.status(200).json({ token, user });
+  } catch (error) {
+    res.status(401).json({ message: error.message });
+  }
+};
+
+export default { register, login };
