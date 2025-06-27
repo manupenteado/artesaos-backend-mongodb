2a4033241a6c9a81501724d9f73684bb57f672e8
Leonardo Andrade <leogalves28@gmail.com>
2025-06-27 17:34:20 +0000

    feat: adiciona verificação de token na rota de produtos

--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import productController from '../controller/product.controller.js';
+import verifyToken from '../middleware/jwt.token.middleware.js';
 
 const router = express.Router();
 
 router.post('/', productController.createProduct);
-router.get('/', productController.getProducts);
+router.get('/', verifyToken, productController.getProducts);
 router.get('/artisan/:artisanId', productController.getProductsByArtisan);
 router.put('/:id', productController.updateProduct);
 router.delete('/:id', productController.deleteProduct);
